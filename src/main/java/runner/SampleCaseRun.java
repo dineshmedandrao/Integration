fb6cdd4936f292856c46e60917ab81359261895b
Dinesh Sagar Medandrao <dineshsagar.medandarao@oracle.com>
2020-05-13 13:22:41 +0530

    Updated POM and added a new file

---
+++ src/main/java/runner/SampleCaseRun.java
@@ -0,0 +1,12 @@
+package runner;
+
+import org.testng.annotations.Test;
+
+public class SampleCaseRun {
+	
+	@Test
+	public void forTesting() {
+		System.out.println("Triggering Second Test case");
+	}
+
+}
